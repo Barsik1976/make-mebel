850b9e36407c967f1da236288b16b7bc927c1629
Barsik1976 <37069851+Barsik1976@users.noreply.github.com>
2021-07-25 14:20:09 +0200

    Add files via upload

---
+++ script-slider.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+
+/*
+
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        effect: "fade",
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+ */
